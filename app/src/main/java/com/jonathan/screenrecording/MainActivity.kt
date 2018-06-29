9e1e9b8fbd187146f20d7413994c905f407719e1
jonathan195628 <jonathan195628@outlook.com>
2018-06-29 11:04:03 +0800

    debugintf

--- app/src/main/java/com/jonathan/screenrecording/MainActivity.kt
+++ app/src/main/java/com/jonathan/screenrecording/MainActivity.kt
@@ -1,44 +1,56 @@
 package com.jonathan.screenrecording
 
-import android.content.Context
+import android.Manifest
+import android.Manifest.permission
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.DisplayMetrics
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    private val instance by lazy { this }
+    private val Tag: String = "MainActivity"
 
-    val Tag: String = "MainActivity"
+    private var isHD: Boolean = false
+    private var watchAndSaveStatus: Int = 1
 
-    var isHD: Boolean = false
-    var watchAndSaveStatus: Int = 1
+    private val REQUESTCODE_STORAGE = 1
 
-    var width: Int = 0
-    var height: Int = 0
-    var fPath:String = ""
+    private var width: Int = 0
+    private var height: Int = 0
+    private var fPath: String = ""
 
-    var mProjectionManager: MediaProjectionManager? = null
-    var mediaProjection: MediaProjection? = null
-    var captureIntent: Intent? = null
+    private var mProjectionManager: MediaProjectionManager? = null
+    private var mediaProjection: MediaProjection? = null
+    private var captureIntent: Intent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         mInit()
+
+        mainfun()
     }
 
-    init {
-        fPath = this.getFilesDir().getAbsolutePath().toString()
+    fun mainfun() {
 
-        getScreenBaseInfo()
+
+
     }
 
     fun mInit() {
+        getScreenBaseInfo()
+
+        fPath = Tools().getDiskCacheDir(instance)
+
         rb_sh.setOnClickListener {
             isHD = false
         }
@@ -76,6 +88,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
     /**
      * 获取屏幕相关数据
      */
@@ -87,5 +100,6 @@ class MainActivity : AppCompatActivity() {
         width = metrics.widthPixels
         height = metrics.heightPixels
     }
+
 
 }
