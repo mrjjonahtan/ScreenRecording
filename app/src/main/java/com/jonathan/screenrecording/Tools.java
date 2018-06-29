adb80a5dfc6ea3b3d330eb7084619131553410f7
jonathan195628 <jonathan195628@outlook.com>
2018-06-29 11:06:56 +0800

    add file

---
+++ app/src/main/java/com/jonathan/screenrecording/Tools.java
@@ -0,0 +1,17 @@
+package com.jonathan.screenrecording;
+
+import android.content.Context;
+import android.os.Environment;
+
+public class Tools {
+    public String getDiskCacheDir(Context context) {
+        String cachePath = null;
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+                || !Environment.isExternalStorageRemovable()) {
+            cachePath = context.getExternalCacheDir().getPath();
+        } else {
+            cachePath = context.getCacheDir().getPath();
+        }
+        return cachePath;
+    }
+}
